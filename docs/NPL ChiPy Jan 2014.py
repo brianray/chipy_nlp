7650484086d6adce38bc9373a9cdfaee7e679a08
Brian Ray <brianhray@gmail.com>
2014-01-07 18:38:30 -0600

    adding notes

---
+++ docs/NPL ChiPy Jan 2014.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# Goals of this talk
+# ==================
+# 
+# * Explore NLTK
+# * Have fun thinking about Lexical Graphs
+# * Explore the posiblity of comparing differences between langaugas using NLP
+# 
+
+# <codecell>
+
+import nltk
+
+# <markdowncell>
+
+# nltk ships with a download utility for downloading grammers and corpora.
+
+# <codecell>
+
+nltk.download()
+
+# <codecell>
+
+nltk.corpus.gutenberg.fileids()
+
+# <codecell>
+
+
+# <codecell>
+
+

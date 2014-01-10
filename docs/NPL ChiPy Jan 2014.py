0fffecca7bb9149f9fb062c607f6b04c043c3522
Brian Ray <brianhray@gmail.com>
2014-01-10 14:28:47 -0600

    updates to final slides

--- docs/NPL ChiPy Jan 2014.py
+++ docs/NPL ChiPy Jan 2014.py
@@ -53,26 +53,35 @@ matches = lambda x, re_parts: any([re.findall(y, x) for y in re_parts])
 def density(lang_code, data):
     if lang_code == 'en':
         content_tags = ("^NN", "^JJ", "^V")    
-    elif lang_code in ('pt', 'es'):
+    elif lang_code == 'pt':
         content_tags = ("\+n", "\+adj", "\+v")
+    elif lang_code == "es":
+        content_tags = ("^v", "^a", "^n")
     return len(filter(lambda x: matches(x[1], content_tags), data)) / float(len(data))
 
 assert density('en', [(1, "NN"), (2, "XX")]) == .5
 assert matches("H+n", ("n", "adj"))
 assert density('pt', [(1, "H+n"), (2, "H+xxx")]) == .5
+assert density('es', [(1, "xxxx"), (2, "vmip3s0")]) == .5
 
 # <markdowncell>
 
+# back to our original example, one phrase is more dense than the other.
+
+# <codecell>
+
+assert density("en", nltk.pos_tag(tokenize("If I were you I wouldn’t do that with these."))) \
+       < density("en", nltk.pos_tag(tokenize("The quick brown fox jumps over the lazy dog.")))
 
 # <markdowncell>
 
 # Tag Spanish Text
 # ================
 
-# <rawcell>
+# <codecell>
 
-# from nltk.corpus import cess_esp
-# sents = cess_esp.tagged_sents()
+from nltk.corpus import cess_esp
+sents = cess_esp.tagged_sents()
 
 # <markdowncell>
 
@@ -92,19 +101,19 @@ test = sents[training_dx+1:]
 
 from nltk import HiddenMarkovModelTagger
 spanish_tagger = HiddenMarkovModelTagger.train(training)
-'accuracy %.1f %%' % (tagger.evaluate(test) * 100)
+'accuracy %.1f %%' % (spanish_tagger.evaluate(test) * 100)
 
 # <codecell>
 
 spanish_tagger.tag(tokenize("A buen entendedor, pocas palabras bastan."))
 
+# <codecell>
+
+spanish_tagger.tag(tokenize("El gato blanco se sentó en la alfombra."))
+
 # <markdowncell>
 
 # Now Portuguese 
-
-# <codecell>
-
-nltk.download()
 
 # <codecell>
 
@@ -122,6 +131,9 @@ port_tagger = HiddenMarkovModelTagger.train(training)
 
 # <markdowncell>
 
+# Cross Language Testing
+# ======================
+# 
 # Language Test data sent from Transifex
 
 # <codecell>
@@ -149,21 +161,121 @@ strings = [dict(
                
                ])]
 
+# <codecell>
+
+import numpy as np
+
+for data_dict in strings:
+    en_density = density("en", nltk.pos_tag(tokenize(data_dict['en_source'])))
+    print "english", en_density
+    es_densities = np.mean([density("es", spanish_tagger.tag(tokenize(x))) for x in data_dict['es']])
+    print "spanish", es_densities
+    pt_densities = np.mean([density("pt", port_tagger.tag(tokenize(x))) for x in data_dict['pt']])
+    print "portuguese", pt_densities
+
+# <codecell>
+
+.59 - .43
+
+# <codecell>
+
+.59 - .37
+
+# <codecell>
+
+.43 - .37
+
 # <markdowncell>
 
-# back to our original example, one phrase is more dense than the other.
+# Other fun stuff
+# ===============
+# http://nltk.org/book/
+# 
+#  	Natural Language Processing with Python
+# --- Analyzing Text with the Natural Language Toolkit
+# 
+# Steven Bird, Ewan Klein, and Edward Loper
+# 
 
 # <codecell>
 
-assert density("en", nltk.pos_tag(tokenize("If I were you I wouldn’t do that with these."))) \
-       < density("en", nltk.pos_tag(tokenize("The quick brown fox jumps over the lazy dog.")))
+from nltk.book import *
 
 # <codecell>
 
+nltk.book.text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
 
-port_tagger.tag(tokenize(strings[0]['pt'][0]))
+# <codecell>
 
+def tabulate(cfdist, words, categories):
+    print '%-16s' % 'Category',
+    for word in words:                                  # column headings
+        print '%6s' % word,
+    print
+    for category in categories:
+        print '%-16s' % category,                       # row heading
+        for word in words:                              # for each word
+            print '%6d' % cfdist[category][word],       # print table cell
+        print                                           # end the row
 
+# <codecell>
+
+from nltk.corpus import brown
+
+cfd = nltk.ConditionalFreqDist(
+          (genre, word)
+          for genre in brown.categories()
+          for word in brown.words(categories=genre))
+genres = ['news', 'religion', 'hobbies', 'science_fiction', 'romance', 'humor']
+modals = ['can', 'could', 'may', 'might', 'must', 'will']
+tabulate(cfd, modals, genres)
+
+# <codecell>
+
+text = nltk.Text(word.lower() for word in nltk.corpus.brown.words())
+text.similar('woman')
+
+# <codecell>
+
+text.similar('bought')
+
+# <markdowncell>
+
+# grammer and logic
+
+# <codecell>
+
+v = """
+bertie => b
+olive => o
+cyril => c
+boy => {b}
+girl => {o}
+dog => {c}
+walk => {o, c}
+see => {(b, o), (c, b), (o, c)}
+"""
+val = nltk.parse_valuation(v)
+g = nltk.Assignment(val.domain)
+m = nltk.Model(val.domain, val)
+sent = 'Cyril sees every boy'
+grammar_file = 'grammars/book_grammars/simple-sem.fcfg'
+results = nltk.batch_evaluate([sent], grammar_file, m, g)[0]
+for (syntree, semrep, value) in results:
+    print semrep
+    print value
+
+# <codecell>
+
+sent = 'Cyril sees a boy'
+results = nltk.batch_evaluate([sent], grammar_file, m, g)[0]
+for (syntree, semrep, value) in results:
+    print semrep
+    print value
+
+# <codecell>
+
+nltk.download()
 
 # <codecell>
 
